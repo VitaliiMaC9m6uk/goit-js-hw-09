4b08e435d6d5675d907b24caa29c4db1161d3a9e
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-03-12 15:47:46 +0100

    зроблене 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let intervalId = null;
 
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', clickInStartBtn);
+stopBtnEl.addEventListener('click', clickInStopBtn);
+
+function clickInStartBtn() {
+    stopBtnEl.disabled = false;
+    startBtnEl.disabled = true;
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+};
+function clickInStopBtn() {
+    clearInterval(intervalId);
+    stopBtnEl.disabled = true;
+    startBtnEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
