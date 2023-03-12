f3b804248341fe31c7a376c4f40e796fb280f14f
VitaliiMaC9m6uk <114355841+VitaliiMaC9m6uk@users.noreply.github.com>
2023-03-12 14:48:13 +0000

    Deploying to gh-pages from @ VitaliiMaC9m6uk/goit-js-hw-09@4b08e435d6d5675d907b24caa29c4db1161d3a9e 🚀

---
+++ 01-color-switcher.6da02634.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=null;e.disabled=!0,t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,n=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.6da02634.js.map
